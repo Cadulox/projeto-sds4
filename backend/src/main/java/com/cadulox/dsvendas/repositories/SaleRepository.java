514593c9ebc40a2894f371c5168f388ee419e31f
Cadulox <cadulox@gmail.com>
2021-09-10 10:11:01 -0300

    Pagination

---
+++ backend/src/main/java/com/cadulox/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,8 @@
+package com.cadulox.dsvendas.repositories;
+
+import com.cadulox.dsvendas.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
